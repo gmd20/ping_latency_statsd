d56c21e54530a5d2841c6a2aaeb626b718cf3eb5
gmd20 <gmd20@163.com>
2014-09-12 14:07:44 +0800

    Create ping_latency_statsd.pl

---
+++ ping_latency_statsd.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl -w
+
+use strict;
+use Net::Ping;
+use IO::Socket;
+
+my $statsd_host   = "192.168.30.60";
+my $statsd_port   = 8125;
+my $ping_dest_ip  = "192.168.30.61";
+my $ping_interval = 1;
+
+my $socket = IO::Socket::INET->new( PeerPort  => $statsd_port,
+                                 PeerAddr  => $statsd_host,
+                                 Type      => SOCK_DGRAM,
+                                 Proto     => 'udp')
+      or die "Socket could not be created, failed with error $!\n";
+
+my $p = Net::Ping->new("icmp");
+$p->hires();
+
+my $hostname = `hostname`;
+chomp($hostname) ;
+$hostname =~ s/ //g;
+my $metric_name = $hostname . ".ping_latency";
+
+while (1) {
+  my ($ret, $duration, $ip) = $p->ping($ping_dest_ip, 5.5);
+  # printf("$host [ip: $ip] is alive (packet return time: %.4f ms)\n",
+  # 1000 * $duration) if $ret;
+  if (not $ret) {
+      $duration = 100000;   # timeout
+  }
+  $duration = $duration *1000;
+  # https://github.com/etsy/statsd/blob/master/docs/metric_types.md
+  my $packet = $metric_name . sprintf (":%.2f|ms", $duration);
+  # print "$packet\n";
+  $socket->send($packet);
+  sleep($ping_interval);
+}
+
+$p->close();
+$socket->close();
